203f5fcd1753963bb54e120c5510a75370202c08
Asdrubal Hernandez <ahernand.development@gmail.com>
2018-12-04 18:33:43 -0500

    Initial Commit

---
+++ SMACSS.md
@@ -0,0 +1,26 @@
+## Modelo de css SMACSS
+
+#### BASE
+
+- Los aplicados a elementos HTML ( no clases, no ID )
+- Casi como un normalize
+- Inicializar tus estilos
+
+#### TEMA
+
+- Look and fell de la web o aplicacion
+
+
+#### ESTADOS
+
+- Cambios de estado.
+    - **.is-hide/.is-show**
+
+#### LAYOUT
+
+- Maquetacion, sin estilos de tema
+
+#### MODULOS
+
+- Los componentes que se repiten a lo largo de la aplicacion
+    - botones, iconos, widgets
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+
+//Tarea para compilar archivos sass a css
+gulp.task('sass', function () {
+    return gulp.src('./scss/*.scss') //Ruta de la carpeta sass apuntando a los archivos `.scss`
+        .pipe(sass().on('error', sass.logError)) //Compila los archivos `.scss` y muestra posibles errores
+        .pipe(gulp.dest('./source/css'))//Carpeta donde se guardaran los archivos `.css` compilado
+        .pipe(notify("Task SCSS Completed!")); //Mensaje gracias al plugin `gulp-notify`
+});
+
+//Vuelve a ejecutar la tarea cuando se modifica algún archivo
+gulp.task('watch', function(){
+    gulp.watch('./scss/**/*', ['sass']);
+});
+
+//Tarea por defecto
+gulp.task('default',['watch', 'sass']);
